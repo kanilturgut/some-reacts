486b53672bdfa9f01596c8bb06cdbeb567b8a1c5
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-09 10:32:34 +0300

    comment-section without components

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
-
-    if (module.hot) {
-        module.hot.accept();
-    }
-
-    return <div>Hi There</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam2
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam3
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))
