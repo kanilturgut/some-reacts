a8a03bff58df83c34e569708a7ff0adba8e55bf2
Kadir Anil Turgut <kanilturgut@gmail.com>
2021-01-09 19:42:15 +0300

    final

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
+import Loader from './Loader'
 
 class App extends React.Component {
 
@@ -8,20 +10,29 @@ class App extends React.Component {
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             position => this.setState({lat: position.coords.latitude}),
+            //err => this.setState({lat: -37})
             err => this.setState({errorMessage: err.message})
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading...</div>
+        return <Loader message="Please accept the location request" />
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
