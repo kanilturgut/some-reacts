9b0cc19d701ca38f083f94834c7393887fd85994
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-09 12:46:21 +0300

    Nested component passign as props

--- components/src/index.js
+++ components/src/index.js
@@ -7,27 +7,40 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Tim"
-                timeAgo="Today at 4:46PM"
-                content="Nice blog"
-                avatar={faker.image.image()}
-            />
-            <CommentDetail
-                author="Tom"
-                timeAgo="Today at 7:00AM"
-                content="Dude it is sick!"
-                avatar={faker.image.image()}
-            />
-            <CommentDetail
-                author="Mark"
-                timeAgo="Yesterday at 6:00PM"
-                content="good"
-                avatar={faker.image.image()}
-            />
-        <ApprovalCard />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Tim"
+                    timeAgo="Today at 4:46PM"
+                    content="Nice blog"
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Tom"
+                    timeAgo="Today at 7:00AM"
+                    content="Dude it is sick!"
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Mark"
+                    timeAgo="Yesterday at 6:00PM"
+                    content="good"
+                    avatar={faker.image.image()}
+                />
+            </ApprovalCard>
         </div>
-        
     );
 };
 
