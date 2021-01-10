7a4908def89eac9219fb1028c72c1f3461aa61b6
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-10 09:54:36 +0300

    axios and unsplash api added

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
     console.log(term);
+
+    axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization: "Client-ID 5HYlPZshDHJ1nCVJu9fIKQb7nHzexbz8E6fu-oddqs8",
+      },
+      params: {
+        query: term,
+      },
+    });
   }
 
   render() {
