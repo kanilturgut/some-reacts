274975bccf36dd652dc4d08244045887301e657c
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-16 13:15:24 +0300

    set default search term, and select gathered videos first index

--- video-search/src/components/VideoDetail.js
+++ video-search/src/components/VideoDetail.js
@@ -8,7 +8,7 @@ const VideoDetail = ({ video }) => {
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return (
-    <div className="ui segment">
+    <div>
       <div className="ui embed">
         <iframe title="video player" src={videoSrc} />
       </div>
