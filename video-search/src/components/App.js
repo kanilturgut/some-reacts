eaa08ebc4bd3f591f856925b6117d072eb3d0f91
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-16 13:02:22 +0300

    displaying selected video

--- video-search/src/components/App.js
+++ video-search/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
@@ -18,13 +19,13 @@ class App extends React.Component {
 
   onVideoSelect = (video) => {
     this.setState({ selectedVideo: video });
-    console.log("From the APP", video);
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
