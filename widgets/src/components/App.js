531a3d72c2599ffc3477266fbe5e1da778f506a5
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-17 07:05:54 +0300

    items added

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import Accordion from "./Accordion";
+
+const items = [
+  {
+    title: "What is react?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use react?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creationg components",
+  },
+];
 
 class App extends React.Component {
   render() {
-    return <h1>Widget App</h1>;
+    return (
+      <div>
+        <Accordion items={items} />
+      </div>
+    );
   }
 }
 
