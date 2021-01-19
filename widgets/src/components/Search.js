086e26dc0c84c13de3204eab25c4594bc5dd9535
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-19 21:58:14 +0300

    setTimeout between keypress and cancel timeout with useEffect hooks cleanup function

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -20,14 +20,29 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    if (term) {
-      search();
-    }
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500);
+
+    // cleanup function
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
