0aeec3ba037b651779e63c3acf861fc57bc4103f
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-27 06:45:35 +0300

    action creator and connect method combined to change state

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { selectSong } from "../actions";
+
 class SongList extends Component {
   renderList() {
     return this.props.songs.map((song) => {
       return (
         <div key={song.title} className="item">
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => {
+                this.props.selectSong(song);
+              }}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -24,4 +33,4 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
