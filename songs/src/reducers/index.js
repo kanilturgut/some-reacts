975378774da5c66bfd56566f99e35e7412efe48c
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-27 06:04:46 +0300

    combineReducers usage in the reducers

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songsReducer = () => {
   return [
     {
@@ -25,3 +27,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   }
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
