ae78cd2eeadb48f52b296417be806f7fd3930c0a
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-28 08:46:30 +0300

    rules of reducers added as comment

--- blog-posts/src/reducers/index.js
+++ blog-posts/src/reducers/index.js
@@ -1,6 +1,20 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
 
+/**
+ * Rules of Reducers
+ *
+ * Must return any valur beside 'undefined'
+ *
+ * Produces 'state', or data to be used inside of your app using
+ * only previous state and the action
+ *
+ * Must not return reach 'out of itself' to decide what value to
+ * return (reducers are pure)
+ *
+ * Must no mutate its input 'state' argument
+ *
+ */
 export default combineReducers({
   posts: postsReducer,
 });
