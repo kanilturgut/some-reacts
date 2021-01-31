598c0b1e189db4c70d52d59348a0473fbd49550c
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-31 14:44:37 +0300

    created fetchPostsAndUsers method to gather posts and related user information with un-repeated request

--- blog-posts/src/actions/index.js
+++ blog-posts/src/actions/index.js
@@ -1,12 +1,27 @@
 import _ from "lodash";
 import jsonPlaceHolder from "../api/jsonplaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
+
 // async action creation with middleware
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceHolder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUser = (userId) => async (dispatch) => {
+  const response = await jsonPlaceHolder.get(`/users/${userId}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+/*
 
 export const fetchUser = (userId) => (dispatch) => _fetchUser(userId, dispatch);
 
@@ -15,3 +30,5 @@ const _fetchUser = _.memoize(async (userId, dispatch) => {
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 });
+
+*/
