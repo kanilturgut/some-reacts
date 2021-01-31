fab740c3b7e6ad4329d4915a977cd494d8b88765
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-01-31 14:28:41 +0300

    using memoize to not make same network request

--- blog-posts/src/actions/index.js
+++ blog-posts/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceHolder from "../api/jsonplaceholder";
 
 // async action creation with middleware
@@ -7,8 +8,10 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (userId) => async (dispatch) => {
+export const fetchUser = (userId) => (dispatch) => _fetchUser(userId, dispatch);
+
+const _fetchUser = _.memoize(async (userId, dispatch) => {
   const response = await jsonPlaceHolder.get(`/users/${userId}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
