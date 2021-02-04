f67454bc3be4aad78d6c483e200d3d39bb486c69
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-02-04 08:39:44 +0300

    redux dev tools

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const componseEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, componseEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
