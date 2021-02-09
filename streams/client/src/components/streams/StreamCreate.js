3350fdecde720edecdfec42c8d6c401bc105072d
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-02-09 08:38:17 +0300

    create steam with new actions

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -24,10 +26,12 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
+  onSubmit = (formValues) => {
     // no need to call manually. Redux-form does it automatically
     // event.preventDefault();
-  }
+
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -61,7 +65,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
