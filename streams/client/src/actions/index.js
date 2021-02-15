fd97a6653c8787ea320fb2ada6b921771cd868d5
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-02-15 07:54:50 +0300

    programmatic navigation with history object

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -9,6 +9,8 @@ import {
   EDIT_STREAM,
 } from "./types";
 
+import history from "../history";
+
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
@@ -27,6 +29,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/"); // programmatic navigation
 };
 
 export const fetchStreams = () => async (dispatch) => {
