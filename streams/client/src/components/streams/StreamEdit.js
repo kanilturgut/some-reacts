c0c6c5ca08b8bbbd6a8688abbfb17a4d718a26ea
Kadir Anıl Turğut <kanilturgut@gmail.com>
2021-02-16 06:51:22 +0300

    initialValues props for ReduxForm

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -19,10 +19,7 @@ class StreamEdit extends React.Component {
       <div>
         <h3>Edit a Stream</h3>
         <StreamForm
-          initialValues={{
-            title: this.props.stream.title,
-            description: this.props.stream.description,
-          }}
+          initialValues={this.props.stream}
           onSubmit={this.onSubmit}
         />
       </div>
